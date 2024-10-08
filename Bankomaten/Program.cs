b58341a84b6750f1eb68b6cf679e1667927058ad
ArbunitShala <arbunitshalaa@gmail.com>
2024-10-08 19:44:25 +0200

    user accounts and pins added, bank accounts and login

--- Bankomaten/Program.cs
+++ Bankomaten/Program.cs
@@ -2,29 +2,60 @@
 {
     internal class Program
     {
-        static string[] users = { "Arbunit", "Petter", "Xhoni", "Hexe", "Krull" };
-        static string[]  userPins = { "2004", "1111", "2222", "3333", "4444"};
         static void Main(string[] args)
         {
+            //tvådimensionell array som sparar användarna och pinkoderna
+            string[,] usersAndPins = new string[5, 2];
+            //jagged array som sparar användarnas konton i banken
+            decimal[][] accounts = new decimal[5][];
+            //array som har namnen på kontorna i banken
+            string[] accountNames = ["Lönekonto", "Sparkonto", "Hushåll", "Buffert", "Barnsparkonto"];
+
+            usersAndPins[0, 0] = "user1";
+            usersAndPins[0, 1] = "1111";
+            usersAndPins[1, 0] = "user2";
+            usersAndPins[1, 1] = "2222";
+            usersAndPins[2, 0] = "user3";
+            usersAndPins[2, 1] = "3333";
+            usersAndPins[3, 0] = "user4";
+            usersAndPins[3, 1] = "4444";
+            usersAndPins[4, 0] = "user5";
+            usersAndPins[4, 1] = "5555";
+
+            accounts[0] = [1100.35m];
+            accounts[1] = [13000m, 25300.59m];
+            accounts[2] = [33549.77m, 177000, 5600m];
+            accounts[3] = [21000, 450000m, 1600m, 9500m];
+            accounts[4] = [65000m, 5000000m, 12000.56m, 50000m, 81000.39m];
+
             Console.WriteLine("Välkomen till bankomaten! ");
-            bool run = true;
-            int attempts = 0;
-            while (attempts < 3)
+            Console.Write("Ange ditt användarnamn: ");
+            string userName = Console.ReadLine();
+
+            int userIndex = -1;
+            for (int i = 0; i < usersAndPins.GetLength(0); i++)
             {
-                Console.Write("Ange ditt användarnamn: ");
-                string userName = Console.ReadLine();
-                Console.Write("Ange din pin-kod: ");
-                string pinCode = Console.ReadLine();
-                if ()
+                if(userName == usersAndPins[i, 0])
                 {
-                    Console.WriteLine("Inloggning lyckades! Klicka på valfri knapp för att gå vidare.");
+                    userIndex = i;
                 }
-                else
+            }
+
+            Console.Write("Ange din pin-kod: ");
+            string userPin = Console.ReadLine();
+
+            if (usersAndPins[userIndex,1] == userPin)
+            {
+                Console.Clear();
+                Console.WriteLine("Du är inloggad ");
+                for(int i = 0; i < accounts[userIndex].Length; i++)
                 {
-                    Console.WriteLine("Fel användarnamn eller Pin-kod. Försök igen.");
-                    attempts++;
+                    Console.WriteLine($"{accountNames[i]}: {accounts[userIndex][i]:C}");
                 }
-            }  
+
+            }
+
+
         }
     }
 }
